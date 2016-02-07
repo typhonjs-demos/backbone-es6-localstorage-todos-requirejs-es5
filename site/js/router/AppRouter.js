fef0bc7024821014900b70d1374e734d9f31ee12
Michael Leahy <support@typhonrt.org>
2016-02-07 13:58:38 -0800

    Update AppRouter.js

--- site/js/router/AppRouter.js
+++ site/js/router/AppRouter.js
@@ -1,13 +1,9 @@
 /**
  * Provides a basic `Backbone.Router` to route between app states: `all`, `active` & `completed`. When a user
- * navigates between these different filter type states `Backbone.history.navigate` is invoked in
- * `App->selectFilter` to update the navigation history allowing the user to use the back and forward browser
- * controls to navigate the app. When a filter type is selected it is referenced in the current URL for the web app.
- * If this URL is navigated to and the user is still logged into the app `AppRouter` will set the `AppState`
- * accordingly and `ManageTodosView` will appropriately show the `Items` for the current user given the filter state
- * set below. If a user is not logged in `App` renders the login view and `AppRouter` will replace any route
- * matches with no hash tag.
- *
+ * navigates between these different filter type states `Backbone.history.navigate` is invoked in `App.js->selectFilter`
+ * to update the navigation history allowing the user to use the back and forward browser controls to navigate the app.
+ * When a filter type is selected it is referenced in the current URL for the web app. 
+ * 
  * Please note that in the `App constructor` a default catch all no route callback is specified which handles
  * unmatched routes.
  */
